0f18a15a8b9725630266efaf10a5983ea5ef4902
donelin <1622441350@qq.com>
2018-03-26 01:38:11 +0800

    Springboot2  案列

---
+++ springboot2-demo/src/main/java/com/done/Springboot2DemoApplication.java
@@ -0,0 +1,12 @@
+package com.done;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2DemoApplication.class, args);
+	}
+}
